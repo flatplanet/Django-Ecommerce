b637972cccba937162b2a32bb6bf0a8e8759533f
John Elder <johne4196@gmail.com?>
2023-09-05 17:16:30 -0700

    Add Template Files

--- ecom/settings.py
+++ ecom/settings.py
@@ -106,6 +106,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = ['static/']
+
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
