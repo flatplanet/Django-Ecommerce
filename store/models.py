aea4b7166ce041c70fbb1dbffdc863e19f5d697d
John Elder <johne4196@gmail.com?>
2023-09-13 08:38:55 -0700

    Product Sales

--- store/models.py
+++ store/models.py
@@ -34,6 +34,9 @@ class Product(models.Model):
 	category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
 	description = models.CharField(max_length=250, default='', blank=True, null=True)
 	image = models.ImageField(upload_to='uploads/product/')
+	# Add Sale Stuff
+	is_sale = models.BooleanField(default=False)
+	sale_price = models.DecimalField(default=0, decimal_places=2, max_digits=6)
 
 	def __str__(self):
 		return self.name
