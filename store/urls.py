dd790950c61c62266994c0aed740260ef026e23b
John Elder <johne4196@gmail.com?>
2023-09-20 09:18:09 -0700

    template out base.html

--- store/urls.py
+++ store/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
 ]
--- store/views.py
+++ store/views.py
@@ -5,3 +5,7 @@ from .models import Product
 def home(request):
 	products = Product.objects.all()
 	return render(request, 'home.html', {'products':products})
+
+
+def about(request):
+	return render(request, 'about.html', {})	
