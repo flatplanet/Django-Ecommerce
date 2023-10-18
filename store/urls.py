6937684adc6a92ed374d656a36030bd86a8c2292
John Elder <johne4196@gmail.com>
2023-10-18 09:37:32 -0700

    category pages

--- store/urls.py
+++ store/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('logout/', views.logout_user, name='logout'),
     path('register/', views.register_user, name='register'),
     path('product/<int:pk>', views.product, name='product'),
+    path('category/<str:foo>', views.category, name='category'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Product
+from .models import Product, Category
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.models import User
@@ -7,6 +7,19 @@ from django.contrib.auth.forms import UserCreationForm
 from .forms import SignUpForm
 from django import forms
 
+
+def category(request, foo):
+	# Replace Hyphens with Spaces
+	foo = foo.replace('-', ' ')
+	# Grab the category from the url
+	try:
+		# Look Up The Category
+		category = Category.objects.get(name=foo)
+		products = Product.objects.filter(category=category)
+		return render(request, 'category.html', {'products':products, 'category':category})
+	except:
+		messages.success(request, ("That Category Doesn't Exist..."))
+		return redirect('home')
 
 
 def product(request,pk):
