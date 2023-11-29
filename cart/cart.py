14d72218712092c8ce06e70543ed93024e422d34
John Elder <johne4196@gmail.com>
2023-11-29 11:15:10 -0800

    product quantities

--- cart/cart.py
+++ cart/cart.py
@@ -1,3 +1,4 @@
+from store.models import Product
 
 class Cart():
 	def __init__(self, request):
@@ -14,17 +15,30 @@ class Cart():
 		# Make sure cart is available on all pages of site
 		self.cart = cart
 
-	def add(self, product):
+	def add(self, product, quantity):
 		product_id = str(product.id)
-
+		product_qty = str(quantity)
 		# Logic
 		if product_id in self.cart:
 			pass
 		else:
-			self.cart[product_id] = {'price': str(product.price)}
+			#self.cart[product_id] = {'price': str(product.price)}
+			self.cart[product_id] = int(product_qty)
 
 		self.session.modified = True
 
 	def __len__(self):
 		return len(self.cart)
 
+	def get_prods(self):
+		# Get ids from cart
+		product_ids = self.cart.keys()
+		# Use ids to lookup products in database model
+		products = Product.objects.filter(id__in=product_ids)
+
+		# Return those looked up products
+		return products
+
+	def get_quants(self):
+		quantities = self.cart
+		return quantities
--- cart/views.py
+++ cart/views.py
@@ -4,7 +4,11 @@ from store.models import Product
 from django.http import JsonResponse
 
 def cart_summary(request):
-	return render(request, "cart_summary.html", {})
+	# Get the cart
+	cart = Cart(request)
+	cart_products = cart.get_prods
+	quantities = cart.get_quants
+	return render(request, "cart_summary.html", {"cart_products":cart_products, "quantities":quantities})
 
 
 
@@ -16,12 +20,13 @@ def cart_add(request):
 	if request.POST.get('action') == 'post':
 		# Get stuff
 		product_id = int(request.POST.get('product_id'))
-		
+		product_qty = int(request.POST.get('product_qty'))
+
 		# lookup product in DB
 		product = get_object_or_404(Product, id=product_id)
 		
 		# Save to session
-		cart.add(product=product)
+		cart.add(product=product, quantity=product_qty)
 
 		# Get Cart Quantity
 		cart_quantity = cart.__len__()
