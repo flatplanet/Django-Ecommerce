fcfc5786dbcf71e364cad900fac5552fcc169d74
John Elder <johne4196@gmail.com>
2023-12-06 10:02:01 -0800

    update cart quantities

--- cart/cart.py
+++ cart/cart.py
@@ -42,3 +42,17 @@ class Cart():
 	def get_quants(self):
 		quantities = self.cart
 		return quantities
+
+	def update(self, product, quantity):
+		product_id = str(product)
+		product_qty = int(quantity)
+
+		# Get cart
+		ourcart = self.cart
+		# Update Dictionary/cart
+		ourcart[product_id] = product_qty
+
+		self.session.modified = True
+
+		thing = self.cart
+		return thing
--- cart/views.py
+++ cart/views.py
@@ -40,4 +40,15 @@ def cart_delete(request):
 	pass
 
 def cart_update(request):
-	pass
+	cart = Cart(request)
+	if request.POST.get('action') == 'post':
+		# Get stuff
+		product_id = int(request.POST.get('product_id'))
+		product_qty = int(request.POST.get('product_qty'))
+
+		cart.update(product=product_id, quantity=product_qty)
+
+		response = JsonResponse({'qty':product_qty})
+		#return redirect('cart_summary')
+		return response
+
