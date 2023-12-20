f11fda267e02a5aac050a7d346d8a7481b26832b
John Elder <johne4196@gmail.com>
2023-12-20 08:30:28 -0800

    cart totals

--- cart/cart.py
+++ cart/cart.py
@@ -27,6 +27,32 @@ class Cart():
 
 		self.session.modified = True
 
+	def cart_total(self):
+		# Get product IDS
+		product_ids = self.cart.keys()
+		# lookup those keys in our products database model
+		products = Product.objects.filter(id__in=product_ids)
+		# Get quantities
+		quantities = self.cart
+		# Start counting at 0
+		total = 0
+		
+		for key, value in quantities.items():
+			# Convert key string into into so we can do math
+			key = int(key)
+			for product in products:
+				if product.id == key:
+					if product.is_sale:
+						total = total + (product.sale_price * value)
+					else:
+						total = total + (product.price * value)
+
+
+
+		return total
+
+
+
 	def __len__(self):
 		return len(self.cart)
 
--- cart/views.py
+++ cart/views.py
@@ -8,7 +8,8 @@ def cart_summary(request):
 	cart = Cart(request)
 	cart_products = cart.get_prods
 	quantities = cart.get_quants
-	return render(request, "cart_summary.html", {"cart_products":cart_products, "quantities":quantities})
+	totals = cart.cart_total()
+	return render(request, "cart_summary.html", {"cart_products":cart_products, "quantities":quantities, "totals":totals})
 
 
 
