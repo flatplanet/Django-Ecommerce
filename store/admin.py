4ee967ef48786c9d1b8a181f36c44eeeb8593f91
John Elder <johne4196@gmail.com>
2024-01-24 09:09:37 -0800

    extend user profile

--- store/admin.py
+++ store/admin.py
@@ -1,7 +1,26 @@
 from django.contrib import admin
-from .models import Category, Customer, Product, Order
+from .models import Category, Customer, Product, Order, Profile
+from django.contrib.auth.models import User
 
 admin.site.register(Category)
 admin.site.register(Customer)
 admin.site.register(Product)
 admin.site.register(Order)
+admin.site.register(Profile)
+
+
+# Mix profile info and user info
+class ProfileInline(admin.StackedInline):
+	model = Profile
+
+# Extend User Model
+class UserAdmin(admin.ModelAdmin):
+	model = User
+	field = ["username", "first_name", "last_name", "email"]
+	inlines = [ProfileInline]
+
+# Unregister the old way
+admin.site.unregister(User)
+
+# Re-Register the new way
+admin.site.register(User, UserAdmin)
--- store/models.py
+++ store/models.py
@@ -1,5 +1,38 @@
 from django.db import models
 import datetime
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+
+
+# Create Customer Profile
+class Profile(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	date_modified = models.DateTimeField(User, auto_now=True)
+	phone = models.CharField(max_length=20, blank=True)
+	address1 = models.CharField(max_length=200, blank=True)
+	address2 = models.CharField(max_length=200, blank=True)
+	city = models.CharField(max_length=200, blank=True)
+	state = models.CharField(max_length=200, blank=True)
+	zipcode = models.CharField(max_length=200, blank=True)
+	country = models.CharField(max_length=200, blank=True)
+
+	def __str__(self):
+		return self.user.username
+
+# Create a user Profile by default when user signs up
+def create_profile(sender, instance, created, **kwargs):
+	if created:
+		user_profile = Profile(user=instance)
+		user_profile.save()
+
+# Automate the profile thing
+post_save.connect(create_profile, sender=User)
+
+
+
+
+
+
 
 # Categories of Products
 class Category(models.Model):
