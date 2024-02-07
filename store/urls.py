b78bc7c04e5b1ee8054fa8999aec44adad9689d6
John Elder <johne4196@gmail.com>
2024-02-07 09:34:17 -0800

    search

--- store/urls.py
+++ store/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path('product/<int:pk>', views.product, name='product'),
     path('category/<str:foo>', views.category, name='category'),
     path('category_summary/', views.category_summary, name='category_summary'),
+    path('search/', views.search, name='search'),
 ]
--- store/views.py
+++ store/views.py
@@ -6,6 +6,23 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .forms import SignUpForm, UpdateUserForm, ChangePasswordForm, UserInfoForm
 from django import forms
+from django.db.models import Q
+
+
+def search(request):
+	# Determine if they filled out the form
+	if request.method == "POST":
+		searched = request.POST['searched']
+		# Query The Products DB Model
+		searched = Product.objects.filter(Q(name__icontains=searched) | Q(description__icontains=searched))
+		# Test for null
+		if not searched:
+			messages.success(request, "That Product Does Not Exist...Please try Again.")
+			return render(request, "search.html", {})
+		else:
+			return render(request, "search.html", {'searched':searched})
+	else:
+		return render(request, "search.html", {})	
 
 
 def update_info(request):
