1bccd68bbc9e0ea05b2a8b75d3c7010f8c1267d0
John Elder <johne4196@gmail.copm>
2024-03-07 09:47:25 -0800

    shipping form

--- payment/forms.py
+++ payment/forms.py
@@ -5,7 +5,7 @@ class ShippingForm(forms.ModelForm):
 	shipping_full_name = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Full Name'}), required=True)
 	shipping_email = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Email Address'}), required=True)
 	shipping_address1 = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Address1'}), required=True)
-	shipping_address2 = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Address2'}), required=True)
+	shipping_address2 = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Address2'}), required=False)
 	shipping_city = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'City'}), required=True)
 	shipping_state = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'State'}), required=False)
 	shipping_zipcode = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Zipcode'}), required=False)
--- payment/models.py
+++ payment/models.py
@@ -7,7 +7,7 @@ class ShippingAddress(models.Model):
 	shipping_full_name = models.CharField(max_length=255)
 	shipping_email = models.CharField(max_length=255)
 	shipping_address1 = models.CharField(max_length=255)
-	shipping_address2 = models.CharField(max_length=255)
+	shipping_address2 = models.CharField(max_length=255, null=True, blank=True)
 	shipping_city = models.CharField(max_length=255)
 	shipping_state = models.CharField(max_length=255, null=True, blank=True)
 	shipping_zipcode = models.CharField(max_length=255, null=True, blank=True)
