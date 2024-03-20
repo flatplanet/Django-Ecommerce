68c46c4692d055fc6b8a6fce699655f2f1c6ab4d
John Elder <johne4196@gmail.copm>
2024-03-20 10:27:15 -0700

    order checkout pt 1

--- payment/urls.py
+++ payment/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('payment_success', views.payment_success, name='payment_success'),
+    path('checkout', views.checkout, name='checkout'),
     
 ]
--- payment/views.py
+++ payment/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render
+from cart.cart import Cart
+from payment.forms import ShippingForm
+from payment.models import ShippingAddress
+
+def checkout(request):
+	# Get the cart
+	cart = Cart(request)
+	cart_products = cart.get_prods
+	quantities = cart.get_quants
+	totals = cart.cart_total()
+
+	if request.user.is_authenticated:
+		# Checkout as logged in user
+		# Shipping User
+		shipping_user = ShippingAddress.objects.get(user__id=request.user.id)
+		# Shipping Form
+		shipping_form = ShippingForm(request.POST or None, instance=shipping_user)
+		return render(request, "payment/checkout.html", {"cart_products":cart_products, "quantities":quantities, "totals":totals, "shipping_form":shipping_form })
+	else:
+		# Checkout as guest
+		shipping_form = ShippingForm(request.POST or None)
+		return render(request, "payment/checkout.html", {"cart_products":cart_products, "quantities":quantities, "totals":totals, "shipping_form":shipping_form})
+
+	
 
 def payment_success(request):
-
 	return render(request, "payment/payment_success.html", {})
