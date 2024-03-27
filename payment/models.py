0d90de4f0f5e1187b369ea65324521dcbcaf06fe
John Elder <johne4196@gmail.copm>
2024-03-27 09:12:45 -0700

    fix shipping problem

--- payment/models.py
+++ payment/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from store.models import Product
+from django.db.models.signals import post_save
 
 class ShippingAddress(models.Model):
 	user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
@@ -20,6 +21,17 @@ class ShippingAddress(models.Model):
 
 	def __str__(self):
 		return f'Shipping Address - {str(self.id)}'
+
+# Create a user Shipping Address by default when user signs up
+def create_shipping(sender, instance, created, **kwargs):
+	if created:
+		user_shipping = ShippingAddress(user=instance)
+		user_shipping.save()
+
+# Automate the profile thing
+post_save.connect(create_shipping, sender=User)
+
+
 
 # Create Order Model
 class Order(models.Model):
