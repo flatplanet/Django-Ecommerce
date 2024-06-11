1ba9356df016b5be32acef394298edd56a33c7d3
John Elder <john@codemy.com>
2024-06-11 09:19:32 -0700

    added postgres db

--- ecom/settings.py
+++ ecom/settings.py
@@ -1,8 +1,12 @@
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Load our environmental variables
+load_dotenv()
 
 
 # Quick-start development settings - unsuitable for production
@@ -69,8 +73,15 @@ WSGI_APPLICATION = 'ecom.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': os.environ.get('DB_PASSWORD_YO'),
+        'HOST': 'viaduct.proxy.rlwy.net',
+        'PORT': '23278',
+        
     }
 }
 
