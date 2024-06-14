09905b7544aa990b73fc03d762685759fd40f08e
Junaid Saleem <junaidsaleem986@gmail.com>
2024-06-14 17:08:39 +0500

    made navbar dropdown dynamic

--- ecom/settings.py
+++ ecom/settings.py
@@ -59,6 +59,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'cart.context_processors.cart',
+                'store.context_processors.categories',
 
             ],
         },
