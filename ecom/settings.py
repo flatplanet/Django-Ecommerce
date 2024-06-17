4719ad0603a612d54a34024de0f15835405b1d5c
John Elder <john@codemy.com>
2024-06-17 10:19:31 -0700

    added DB Password environ

--- ecom/settings.py
+++ ecom/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'railway',
         'USER': 'postgres',
-        'PASSWORD': os.environ.get('DB_PASSWORD_YO'),
+        'PASSWORD': os.environ['DB_PASSWORD_YO'],
         'HOST': 'viaduct.proxy.rlwy.net',
         'PORT': '23278',
         
